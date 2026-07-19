b3910818dfe79ba15b97e110b53eac6749f311e2
Al Aiham <ayhem9987@gmail.com>
2026-07-19 12:16:30 +0400

    Added Pharmacy Model

--- Pharmacy System/Models/Pharmacy.cs
+++ Pharmacy System/Models/Pharmacy.cs
@@ -1,6 +1,23 @@
-﻿namespace Pharmacy_System.Modules
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharmacy_System.Modules
 {
     public class Pharmacy
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PharmacyID { get; set; }
+
+        [Required, MaxLength(250)]
+        public string Location { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Email { get; set; }
+
+        [Required, MaxLength(8)]
+        public string Phone {  get; set; }
+
+        [Required]
+        public int StorageCapacity { get; set; }
     }
 }
