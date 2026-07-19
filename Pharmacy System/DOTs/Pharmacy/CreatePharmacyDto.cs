47e4c65b6c475973b4ac3d2c12bb562c0c70b61d
Al Aiham <ayhem9987@gmail.com>
2026-07-19 17:08:49 +0400

    Added Pharmacy DTO

--- Pharmacy System/DOTs/Pharmacy/CreatePharmacyDto.cs
+++ Pharmacy System/DOTs/Pharmacy/CreatePharmacyDto.cs
@@ -1,6 +1,18 @@
-﻿namespace Pharmacy_System.DOTs.Pharmacy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DOTs.Pharmacy
 {
     public class CreatePharmacyDto
     {
+        [Required]
+        public string PharmacyName { get; set; } = string.Empty;
+
+        [Required]
+        public string Location { get; set; } = string.Empty;
+
+        [Required]
+        public string Phone {  get; set; } = string.Empty;
+
+        public int StorageCapacity { get; set; }
     }
 }
