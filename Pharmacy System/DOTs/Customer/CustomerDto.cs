85cd335526a1b25a4d7de9167e8b7db0a486bc77
Al Aiham <ayhem9987@gmail.com>
2026-07-19 17:09:17 +0400

    Added Customer DTO

---
+++ Pharmacy System/DOTs/Customer/CustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Pharmacy_System.DOTs.Customer
+{
+    public class CustomerDto
+    {
+        public int CustomerID { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public DateOnly DOB { get; set; }
+    }
+}
