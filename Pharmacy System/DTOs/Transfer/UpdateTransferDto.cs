0cd6605f9fea02c61615b877bfb8663793bcbc4c
Baraah <alsadibaraah@gmail.com>
2026-07-20 10:56:27 +0400

    added transfer DTOs

---
+++ Pharmacy System/DTOs/Transfer/UpdateTransferDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DTOs.Transfer
+{
+    // Used to update the shipment status and receiving date
+    public class UpdateTransferDto
+    {
+        // Current shipment status
+        [Required]
+        public string Status { get; set; } = string.Empty;
+
+        // Date when the pharmacy received the shipment
+        public DateTime? ReceiveDate { get; set; }
+    }
+}
