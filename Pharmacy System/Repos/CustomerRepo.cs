e725a753641e8179814a0d905d5129827a476e3c
Al Aiham <ayhem9987@gmail.com>
2026-07-20 09:16:29 +0400

    Added Customer Repo

--- Pharmacy System/Repos/CustomerRepo.cs
+++ Pharmacy System/Repos/CustomerRepo.cs
@@ -1,6 +1,41 @@
-﻿namespace Pharmacy_System.Repos
+﻿using Pharmacy_System.Modules;
+
+namespace Pharmacy_System.Repos
 {
     public class CustomerRepo
     {
+        private PharmacyContext context;
+
+        public CustomerRepo(PharmacyContext _context)
+        {
+            context = _context;
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            return context.customers.ToList();
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return context.customers.FirstOrDefault(p => p.CustomerID == id);
+        }
+
+        public void Add(Customer customers)
+        {
+            context.customers.Add(customers);
+            context.SaveChanges();
+        }
+
+        public void CustomerUpdate()
+        {
+            context.SaveChanges();
+        }
+
+        public void CustomerDelete(Customer customers)
+        {
+            context.customers.Remove(customers);
+            context.SaveChanges();
+        }
     }
 }
