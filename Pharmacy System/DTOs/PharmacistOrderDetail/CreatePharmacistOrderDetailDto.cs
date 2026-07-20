e1d6c9b9ab97375c121d67f7997a01495a82e67c
Baraah <alsadibaraah@gmail.com>
2026-07-20 14:51:14 +0400

    added PharmacistOrderDetail DTOs

---
+++ Pharmacy System/DTOs/PharmacistOrderDetail/CreatePharmacistOrderDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DTOs.PharmacistOrderDetail
+{
+    // Represents  medicine  inside a pharmacist order
+    // The full pharmacist order can contain multiple medicine 
+    public class CreatePharmacistOrderDetailDto
+    {
+        // ID of the medicine for this order 
+        [Range(1, int.MaxValue)]
+        public int MedicineId { get; set; }
+
+        // Quantity requested 
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
