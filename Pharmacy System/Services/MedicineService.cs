2f4709ff2fe8ddd92858b0b30de071812843d449
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-20 15:00:05 +0400

    Fixing IDs

--- Pharmacy System/Services/MedicineService.cs
+++ Pharmacy System/Services/MedicineService.cs
@@ -1,6 +1,38 @@
-﻿namespace Pharmacy_System.Services
+﻿using Pharmacy_System.DTOs.Medicine;
+using Pharmacy_System.Modules;
+using Pharmacy_System.Repos;
+
+namespace Pharmacy_System.Services
 {
     public class MedicineService
     {
+        private MedicineRepo medicineRepo;
+        private WarehouseRepo warehouseRepo;
+
+       
+        public MedicineService(MedicineRepo _medicineRepo, WarehouseRepo _warehouseRepo)  // Receive the repositories
+        {
+            medicineRepo = _medicineRepo;
+            warehouseRepo = _warehouseRepo;
+        }
+
+       
+        public List<MedicineDto> GetAllMedicines()       // Get all medicines and convert them to MedicineDto
+        {
+            List<Medicine> medicines = medicineRepo.GetAllMedicine();
+
+            return medicines.Select(m => new MedicineDto
+            {
+                MedicineID = m.MedicineID,
+                MedicineName = m.MedicineName,
+                Category = m.Category,
+                UnitPrice = m.UnitPrice,
+                IsAvailable = m.isAvailable
+            }).ToList();
+        }
+
+
+
+
     }
 }
