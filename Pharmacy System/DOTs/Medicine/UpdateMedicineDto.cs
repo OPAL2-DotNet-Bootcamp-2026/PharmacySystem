e9219eb6aed3f357ace721bbbe21067aa92d3bec
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-20 10:00:54 +0400

    Added medicine DTOs : UpdateMedicineDto

--- Pharmacy System/DOTs/Medicine/UpdateMedicineDto.cs
+++ Pharmacy System/DOTs/Medicine/UpdateMedicineDto.cs
@@ -1,6 +1,23 @@
-﻿namespace Pharmacy_System.DOTs.Medicine
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharmacy_System.DOTs.Medicine
 {
     public class UpdateMedicineDto
     {
+
+        [Required]
+        [MaxLength(100)]
+        public string MedicineName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Category { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal UnitPrice { get; set; }
+
+        public bool IsAvailable { get; set; }
+
     }
 }
