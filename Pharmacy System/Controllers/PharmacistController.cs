34fcded9dd3bf124de3bfb1a59e3e820d595354f
Al Aiham <ayhem9987@gmail.com>
2026-07-20 12:35:02 +0400

    Added Pharmacist Controller

--- Pharmacy System/Controllers/PharmacistController.cs
+++ Pharmacy System/Controllers/PharmacistController.cs
@@ -1,24 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Pharmacy_System.Modules;
+using Pharmacy_System.DTOs.Pharmacist;
 using Pharmacy_System.Services;
 
 namespace Pharmacy_System.Controllers
 {
-    [ApiController]
-    [Route("pharmacist")]
+    [ApiController, Route("api/[controller]")]
     public class PharmacistController : ControllerBase
     {
-        private PharmacistService pharmacistService;
+        private readonly PharmacistService pharmacistService;
 
         public PharmacistController(PharmacistService _pharmacistService)
         {
             pharmacistService = _pharmacistService;
         }
 
-        [HttpPost("register")]
-        public IActionResult Register([FromBody] RegisterDto dto)
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            PharmacistResponseDto created = pharmacistService.Register(dto);
+            return Ok(pharmacistService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByid(int id)
+        {
+            var pharmacist = pharmacistService.GetById(id);
+
+            if (pharmacist == null)
+                return NotFound();
+
+            return Ok(pharmacist);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreatePharmacistDto dto)
+        {
+            var pharmacist = pharmacistService.Add(dto);
+
+            return CreatedAtAction(nameof(GetByid), new { id = pharmacist.PharmacistID }, pharmacist); 
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, UpdatePharmacistDto dto)
+        {
+            var pharmacist = pharmacistService.Update(id, dto);
+
+            if (pharmacist == null)
+                return NotFound();
+
+            return Ok(pharmacist);
         }
     }
 }
