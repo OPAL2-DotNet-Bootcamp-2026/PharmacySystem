0c30122982d463ac1c794e3ef204b2521fb1b0af
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-20 10:09:03 +0400

    Added Supplier DTOs : SupplierDto

--- Pharmacy System/DOTs/Supplier/SupplierDto.cs
+++ Pharmacy System/DOTs/Supplier/SupplierDto.cs
@@ -2,5 +2,15 @@
 {
     public class SupplierDto
     {
+
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+
+
+
+
     }
 }
