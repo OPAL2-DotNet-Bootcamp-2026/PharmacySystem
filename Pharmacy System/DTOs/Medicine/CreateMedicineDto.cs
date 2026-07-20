96e108259726f638f00317ceb7e55ac898c33847
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-20 09:46:46 +0400

    Added Medicine DTOs

---
+++ Pharmacy System/DTOs/Medicine/CreateMedicineDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DTOs.Medicine
+{
+    public class CreateMedicineDto  // for adding a new medicine
+    {
+        [Required]
+        public string MedicineName { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+    }
+}
