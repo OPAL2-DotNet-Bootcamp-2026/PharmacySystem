0b495e6ec59173bbbacaecdbc89cee22dff269c9
Al Aiham <ayhem9987@gmail.com>
2026-07-20 10:47:23 +0400

    Fixing spling

--- Pharmacy System/Controllers/PharmacistController.cs
+++ Pharmacy System/Controllers/PharmacistController.cs
@@ -1,6 +1,24 @@
-﻿namespace Pharmacy_System.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Pharmacy_System.Modules;
+using Pharmacy_System.Services;
+
+namespace Pharmacy_System.Controllers
 {
-    public class PharmacistController
+    [ApiController]
+    [Route("pharmacist")]
+    public class PharmacistController : ControllerBase
     {
+        private PharmacistService pharmacistService;
+
+        public PharmacistController(PharmacistService _pharmacistService)
+        {
+            pharmacistService = _pharmacistService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDto dto)
+        {
+            PharmacistResponseDto created = pharmacistService.Register(dto);
+        }
     }
 }
