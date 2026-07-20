1f3ed80c7c17e034b4de815c6d135deef3cd4489
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-20 10:36:47 +0400

    Added warehouse DTOs : UpdateWarehouseDto

--- Pharmacy System/DOTs/Warehouse/UpdateWarehouseDto.cs
+++ Pharmacy System/DOTs/Warehouse/UpdateWarehouseDto.cs
@@ -1,6 +1,19 @@
-﻿namespace Pharmacy_System.DOTs.Warehouse
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DOTs.Warehouse
 {
     public class UpdateWarehouseDto
     {
+        [Required]
+        public string Location { get; set; } = string.Empty;
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string ExpiryDate { get; set; }
     }
+
+
+}
 }
