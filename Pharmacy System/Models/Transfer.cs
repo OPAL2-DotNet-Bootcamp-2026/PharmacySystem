68b1667ea44a1044ff46c37c9aac25ecae19bf21
Baraah <alsadibaraah@gmail.com>
2026-07-20 10:11:41 +0400

    adde some comment in transfer Modules

--- Pharmacy System/Models/Transfer.cs
+++ Pharmacy System/Models/Transfer.cs
@@ -1,4 +1,3 @@
-using Pharmacy_System.Modules;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,33 +9,32 @@ namespace Pharmacy_System.Modules
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int TransferId { get; set; }
 
-
-        // FROM WAREHOUSE
+        // Foreign key for the warehouse sending the medicines
         [Range(1, int.MaxValue)]
         public int WarehouseId { get; set; }
 
+        // Navigation property for the source warehouse
         public Warehouse Warehouse { get; set; } = null!;
 
-
-        // TO PHARMACY
+        // Foreign key for the pharmacy receiving the medicines
         [Range(1, int.MaxValue)]
         public int PharmacyId { get; set; }
 
+        // Navigation property for the destination pharmacy
         public Pharmacy Pharmacy { get; set; } = null!;
 
-
+        // Date when the transfer was created
         public DateTime TransferDate { get; set; } = DateTime.Now;
 
+        // Date when the pharmacy received the transfer
         public DateTime? ReceiveDate { get; set; }
 
-
+        // Current status of the transfer
         [Required]
         public string Status { get; set; } = "Pending";
 
-
-
-
         // Many-to-Many relationship with Medicine
-        public ICollection<Medicine> Medicines { get; set; } = new List<Medicine>();
+        public ICollection<Medicine> Medicines { get; set; }
+            = new List<Medicine>();
     }
 }
