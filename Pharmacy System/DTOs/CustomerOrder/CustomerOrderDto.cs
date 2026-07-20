c132c16394a4b5e4d9c8084b65aa340a10e44855
Baraah <alsadibaraah@gmail.com>
2026-07-20 12:26:03 +0400

    added CustomerOrder DTOs

---
+++ Pharmacy System/DTOs/CustomerOrder/CustomerOrderDto.cs
@@ -0,0 +1,33 @@
+﻿using Pharmacy_System.DTOs.CustomerOrderDetail;
+
+namespace Pharmacy_System.DTOs.CustomerOrder
+{
+    // Used to return complete customer order information from the API
+    public class CustomerOrderDto
+    {
+     
+        public int CustomerOrderId { get; set; }
+
+        // ID of the customer who placed  order
+        public int CustomerId { get; set; }
+
+        // Customer name displayed in the response
+        public string CustomerName { get; set; } = string.Empty;
+
+        // ID of the pharmacy handling the order
+        public int PharmacyId { get; set; }
+
+        // Date when the order  created
+        public DateTime OrderDate { get; set; }
+
+        // Total cost of all medicine items in the order
+        public decimal TotalCost { get; set; }
+
+        // Current status of  order
+        public string Status { get; set; } = string.Empty;
+
+        // List of all medicine items included in the order
+        public List<CustomerOrderDetailDto> OrderDetails { get; set; }= new List<CustomerOrderDetailDto>();
+            
+    }
+}
---
+++ Pharmacy System/DTOs/CustomerOrder/UpdateCustomerOrderDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DTOs.CustomerOrder
+{
+    // Used to update the current status of a customer order
+    public class UpdateCustomerOrderStatusDto
+    {
+        // New order status ==> such as Pending, Completed, or Cancelled
+        [Required]
+        public string Status { get; set; } = string.Empty;
+    }
+}
