9ddd058328a615c347c6204d5d02199244e520d9
Baraah <alsadibaraah@gmail.com>
2026-07-20 15:14:20 +0400

    added PharmacistOrder DTOs

---
+++ Pharmacy System/DTOs/PharmacistOrder/CreatePharmacistOrderDto.cs
@@ -0,0 +1,24 @@
+﻿using Pharmacy_System.DTOs.PharmacistOrderDetail;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_System.DTOs.PharmacistOrder
+{
+    // Used when a pharmacist creates a new medicine request
+    // for the pharmacy
+    public class CreatePharmacistOrderDto
+    {
+        // ID of the pharmacist creating the order
+        [Range(1, int.MaxValue)]
+        public int PharmacistId { get; set; }
+
+        // ID of the pharmacy that needs the medicines
+        [Range(1, int.MaxValue)]
+        public int PharmacyId { get; set; }
+
+        // List of medicines and required quantities
+        [Required]
+        [MinLength(1)]
+        public List<CreatePharmacistOrderDetailDto> OrderDetails { get; set; } = new List<CreatePharmacistOrderDetailDto>();
+           
+    }
+}
---
+++ Pharmacy System/DTOs/PharmacistOrder/PharmacistOrderDto.cs
@@ -0,0 +1,31 @@
+﻿using Pharmacy_System.DTOs.PharmacistOrderDetail;
+
+namespace Pharmacy_System.DTOs.PharmacistOrder
+{
+    // Used to return complete pharmacist order information from  API
+    
+    public class PharmacistOrderDto
+    {
+        // Order ID 
+        public int PharmacistOrderId { get; set; }
+
+        // ID of the pharmacist who created  order
+        public int PharmacistId { get; set; }
+
+        // Name of the pharmacist displayed in  response
+        public string PharmacistName { get; set; } = string.Empty;
+
+        // ID of the pharmacy that requested  medicines
+        public int PharmacyId { get; set; }
+
+        // Date when the order was created
+        public DateTime OrderDate { get; set; }
+
+        // Current status of order
+        public string Status { get; set; } = string.Empty;
+
+        // List of all medicines and quantities included in  order
+        public List<PharmacistOrderDetailDto> OrderDetails { get; set; }  = new List<PharmacistOrderDetailDto>();
+          
+    }
+}
