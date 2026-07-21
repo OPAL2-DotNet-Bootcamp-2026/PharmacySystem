f6496aa95404f5da0de9edb7673cbfa1c5f4a3b8
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-21 10:04:49 +0400

    Added unique for email

--- Pharmacy System/Models/User.cs
+++ Pharmacy System/Models/User.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pharmacy_System.Models
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class User
     {
 
