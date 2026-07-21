469d681260de9276e05aa7e4a7243a5da5cc32e7
Al Aiham <ayhem9987@gmail.com>
2026-07-21 11:21:27 +0400

    Updating CustomerModel

--- Pharmacy System/Models/Customer.cs
+++ Pharmacy System/Models/Customer.cs
@@ -1,20 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Pharmacy_System.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pharmacy_System.Modules
 {
-    public class Customer
+    public class Customer : BaseEntity
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CustomerID { get; set; }             // System Generated
 
-        [Required, MaxLength(150)]
+        [Required, MaxLength(100)]
         public string FullName { get; set; }            // User Input
 
-        [Required, MaxLength(150)]
+        [Required, MaxLength(8)]
         public string Phone {  get; set; }              // User Input
 
+        [MaxLength(100)]
+        public string? Email { get; set; }              // User Input
+
         [Required]
-        public DateOnly DOB { get; set; }               // System Auto Value
+        public DateOnly DOB { get; set; }               // User Input
+
+        [Required]
+        public bool IsActive { get; set; } = true;      // System Default Value
     }
 }
