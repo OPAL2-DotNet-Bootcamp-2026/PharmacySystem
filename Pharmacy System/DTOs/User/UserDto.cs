7140cf22fedcfec3b732874312b301d6616471f7
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-21 14:40:50 +0400

    added DTOs for user : UserDto

--- Pharmacy System/DTOs/User/UserDto.cs
+++ Pharmacy System/DTOs/User/UserDto.cs
@@ -2,5 +2,13 @@
 {
     public class UserDto
     {
+
+        public int UserID { get; set; }     
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
     }
 }
