5a74846f70731d6193526f2def26add5118a4b14
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-21 11:47:27 +0400

    Updated UpdateWarehouseDto

--- Pharmacy System/DTOs/Warehouse/UpdateWarehouseDto.cs
+++ Pharmacy System/DTOs/Warehouse/UpdateWarehouseDto.cs
@@ -4,16 +4,10 @@ namespace Pharmacy_System.DTOs.Warehouse
 {
     public class UpdateWarehouseDto
     {
-        [Required]
+        [Required(ErrorMessage = "Warehouse location is required")]
+        [MaxLength(150)]
         public string Location { get; set; } = string.Empty;
-
-        [Required]
-        public int Quantity { get; set; }
-
-        [Required]
-        public string ExpiryDate { get; set; }
     }
-
 
 }
 
