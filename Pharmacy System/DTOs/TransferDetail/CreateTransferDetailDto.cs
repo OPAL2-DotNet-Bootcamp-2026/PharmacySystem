b6c5bb511c459ea17b7a2993ec5a1b71a9a77930
Baraah <alsadibaraah@gmail.com>
2026-07-21 15:15:19 +0400

    UPDATE TransferDetailDto

--- Pharmacy System/DTOs/TransferDetail/CreateTransferDetailDto.cs
+++ Pharmacy System/DTOs/TransferDetail/CreateTransferDetailDto.cs
@@ -5,12 +5,12 @@ namespace Pharmacy_System.DTOs.TransferDetail
     // Represents one medicine included in the transfer
     public class CreateTransferDetailDto
     {
-        // Input: selected medicine
-        [Range(1, int.MaxValueErrorMessage = "A valid medicine must be selected")],
+      
+        [Range(1, int.MaxValue ,ErrorMessage = "A valid medicine must be selected")],
         public int MedicineID { get; set; }    
         
 
-        // Input
+      
         [Range(1, int.MaxValue,ErrorMessage = "Quantity must be greater than 0")]
         public int Quantity { get; set; }    
         
