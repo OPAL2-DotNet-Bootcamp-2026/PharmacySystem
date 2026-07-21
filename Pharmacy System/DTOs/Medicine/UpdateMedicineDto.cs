78877359ac667f22c3a97c3c5a041393addf54bc
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-21 11:24:04 +0400

    Updated UpdateMedicineDto

--- Pharmacy System/DTOs/Medicine/UpdateMedicineDto.cs
+++ Pharmacy System/DTOs/Medicine/UpdateMedicineDto.cs
@@ -6,15 +6,15 @@ namespace Pharmacy_System.DTOs.Medicine
     public class UpdateMedicineDto
     {
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Medicine name is required")]
+        [MaxLength(100, ErrorMessage = "Medicine name cannot exceed 150 characters")]
         public string MedicineName { get; set; } = string.Empty;
 
-        [Required]
-        [MaxLength(50)]
-        public string Category { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Category is required")]
+        public int CategoryID { get; set; } //fk
 
-        [Column(TypeName = "decimal(10,2)")]
+        [Range(typeof(decimal), "0.01", "10000.00",
+           ErrorMessage = "Unit price must be between 0.01 and 10,000")]
         public decimal UnitPrice { get; set; }
 
         public bool IsAvailable { get; set; }
