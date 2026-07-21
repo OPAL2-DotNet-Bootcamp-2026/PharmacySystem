1c01def2f02c2ff58589e591b965ef303920da24
Baraah <alsadibaraah@gmail.com>
2026-07-21 10:58:29 +0400

    update transfer model

--- Pharmacy System/Models/Transfer.cs
+++ Pharmacy System/Models/Transfer.cs
@@ -5,36 +5,47 @@ namespace Pharmacy_System.Modules
 {
     public class Transfer
     {
+        // system Generated
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int TransferId { get; set; }
 
-        // Foreign key for the warehouse sending the medicines
+        // Input ==> selected warehouse "sending" the medicines
+        [ForeignKey(nameof(Warehouse))]
         [Range(1, int.MaxValue)]
         public int WarehouseID { get; set; }
-
-        // Navigation property for the source warehouse
         public Warehouse Warehouse { get; set; } = null!;
 
-        // Foreign key for the pharmacy receiving the medicines
-        [Range(1, int.MaxValue)]
-        public int PharmacyId { get; set; }
 
-        // Navigation property for the destination pharmacy
+        // input ==> selected pharmacy "receiving" the medicines
+        [ForeignKey(nameof(Pharmacy))]
+        [Range(1, int.MaxValue)]
+        public int PharmacyID { get; set; }
         public Pharmacy Pharmacy { get; set; } = null!;
 
-        // Date when the transfer was created
+
+     
+        // pharmacist order fulfilled by this transfer
+        [ForeignKey(nameof(PharmacistOrder))]
+        public int PharmacistOrderId { get; set; }
+        public PharmacistOrder PharmacistOrder { get; set; }
+
+
+        // Generated automatically when  transfer  created
         public DateTime TransferDate { get; set; } = DateTime.Now;
 
-        // Date when the pharmacy received the transfer
+        // Updated when the pharmacy receives the transfer
+        //will be Null when transfer not received
         public DateTime? ReceiveDate { get; set; }
 
-        // Current status of the transfer
+        //  default value
+        // Can be updated to Shipped, Received or Cancelled
         [Required]
+        [MaxLength(30)]
         public string Status { get; set; } = "Pending";
 
-        // Many-to-Many relationship with Medicine
-        public ICollection<Medicine> Medicines { get; set; }
-            = new List<Medicine>();
+        // Contains the medicines and quantities included in the transfer
+        public ICollection<TransferDetail> TransferDetails { get; set; } = new List<TransferDetail>();
+           
     }
 }
