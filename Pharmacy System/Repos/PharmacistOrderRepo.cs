29bae790e2589b699a19586901423868bc5a8c07
Baraah <alsadibaraah@gmail.com>
2026-07-21 09:24:42 +0400

    added PharmacistOrderRepo

--- Pharmacy System/Repos/PharmacistOrderRepo.cs
+++ Pharmacy System/Repos/PharmacistOrderRepo.cs
@@ -1,6 +1,55 @@
-﻿namespace Pharmacy_System.Repos
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy_System.Modules;
+
+namespace Pharmacy_System.Repos
 {
     public class PharmacistOrderRepo
     {
+        private PharmacyContext context;
+
+        public PharmacistOrderRepo(PharmacyContext _context)
+        {
+            context = _context;
+        }
+
+        // Returns all pharmacist orders with related data
+        public List<PharmacistOrder> GetAllPharmacistOrders()
+        {
+            return context.PharmacistOrders
+                .Include(o => o.Pharmacist)
+                .Include(o => o.Pharmacy)
+                .Include(o => o.PharmacistOrderDetails).ThenInclude(d => d.Medicine)
+                .ToList();
+        }
+
+        // Returns one pharmacist order using its ID
+        public PharmacistOrder? GetPharmacistOrderById(int id)
+        {
+            return context.PharmacistOrders
+                .Include(o => o.Pharmacist)
+                .Include(o => o.Pharmacy)
+                .Include(o => o.PharmacistOrderDetails).ThenInclude(d => d.Medicine)
+                .FirstOrDefault(o => o.PharmacistOrderId == id);
+        }
+
+        // Adds new pharmacist order
+        public void Add(PharmacistOrder pharmacistOrder)
+        {
+            context.PharmacistOrders.Add(pharmacistOrder);
+            context.SaveChanges();
+        }
+
+        // Save changes made to  pharmacist order
+        public void PharmacistOrderUpdate()
+        {
+            context.SaveChanges();
+        }
+
+        // Delete  pharmacist order
+        public void PharmacistOrderDelete(PharmacistOrder pharmacistOrder)
+        {
+            context.PharmacistOrders.Remove(pharmacistOrder);
+            context.SaveChanges();
+        }
     }
 }
