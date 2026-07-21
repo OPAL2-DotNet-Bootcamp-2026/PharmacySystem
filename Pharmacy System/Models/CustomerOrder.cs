6789fbbb6b3423839e2db481d6d32fdf6bc08ca8
Baraah <alsadibaraah@gmail.com>
2026-07-21 11:03:31 +0400

    update CustomerOrder model

--- Pharmacy System/Models/CustomerOrder.cs
+++ Pharmacy System/Models/CustomerOrder.cs
@@ -1,36 +1,44 @@
-﻿using Pharmacy_System.Modules;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pharmacy_System.Modules
 {
     public class CustomerOrder
     {
+        // system Generated 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CustomerOrderId { get; set; }
 
+        // input ==> selected customer
+        [ForeignKey(nameof(Customer))]
         [Range(1, int.MaxValue)]
-        public int CustomerId { get; set; }//FK
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
 
-        public Customer Customer { get; set; } = null!;//RELATION
 
+        // input==> selected pharmacy fulfilling the order
+         [ForeignKey(nameof(Pharmacy))]
         [Range(1, int.MaxValue)]
-        public int PharmacyId { get; set; }//FK
+        public int PharmacyId { get; set; }
+        public Pharmacy Pharmacy { get; set; } = null!;
 
-        public Pharmacy Pharmacy { get; set; } = null!;//RELATION
-
+        //system generated  when order is created
         public DateTime OrderDate { get; set; } = DateTime.Now;
 
-        [Column(TypeName = "decimal(12,2)")]
+        // Calculated
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(typeof(decimal), "0.00", "999999.99")]
         public decimal TotalCost { get; set; }
 
+
+        // default value: Can be updated to :Completed or Cancelled
         [Required]
+        [MaxLength(30)]
         public string Status { get; set; } = "Pending";
 
-        //RELATION
-        public ICollection<CustomerOrderDetail> CustomerOrderDetails{ get; set; } = new List<CustomerOrderDetail>();
-      
-
+        // Contains all medicines included in customer order
+        public ICollection<CustomerOrderDetail> CustomerOrderDetails { get; set; }= new List<CustomerOrderDetail>();
+            
     }
 }
