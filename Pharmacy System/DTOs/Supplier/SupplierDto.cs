0ddcae198414fe2bdba2409f3d9688ee8ed447da
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-21 11:40:53 +0400

    updated UpdateSupplierDto

--- Pharmacy System/DTOs/Supplier/SupplierDto.cs
+++ Pharmacy System/DTOs/Supplier/SupplierDto.cs
@@ -8,8 +8,9 @@
         public string Phone { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
-
-
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
 
 
     }
--- Pharmacy System/DTOs/Supplier/UpdateSupplierDto.cs
+++ Pharmacy System/DTOs/Supplier/UpdateSupplierDto.cs
@@ -5,20 +5,21 @@ namespace Pharmacy_System.DTOs.Supplier
     public class UpdateSupplierDto
     {
 
-        [Required]
+        [Required(ErrorMessage = "Supplier name is required")]
         [MaxLength(100)]
         public string FullName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Phone number is required")]
 
-        [Required]
         public string Phone { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
         [MaxLength(100)]
         [RegularExpression(@".+@.+\.com$", ErrorMessage = "Email must contain @ and end with .com")]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
-        [MaxLength(200)]
+        [Required(ErrorMessage = "Location is required")]
+        [MaxLength(150)]
         public string Location { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
 
     }
 }
