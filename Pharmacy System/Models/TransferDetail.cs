d280ca6b8252ca6cca759b72b359a50fc9e6646c
Baraah <alsadibaraah@gmail.com>
2026-07-21 15:10:13 +0400

    added transferdetail

--- Pharmacy System/Models/TransferDetail.cs
+++ Pharmacy System/Models/TransferDetail.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class TransferDetails
+namespace Pharmacy_System.Modules
 {
-	public Class1()
-	{
-	}
+    public class TransferDetail : BaseEntity
+    {
+        [Index(nameof(TransferID), nameof(MedicineID), IsUnique = true)]
+        //system generated 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransferDetailID { get; set; }
+
+       
+        [ForeignKey(nameof(Transfer))]
+        [Range(1, int.MaxValue)]
+        public int TransferID { get; set; }
+        public Transfer Transfer { get; set; } = null!;
+
+      
+        [ForeignKey(nameof(Medicine))]
+        [Range(1, int.MaxValue)]
+        public int MedicineID { get; set; }
+        public Medicine Medicine { get; set; } = null!;
+
+       
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
 }
