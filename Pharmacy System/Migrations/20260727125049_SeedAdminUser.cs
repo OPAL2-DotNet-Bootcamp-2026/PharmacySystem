00290aa69a959adb42226bb2edd7e699dac106b6
Al Aiham <ayhem9987@gmail.com>
2026-07-27 17:02:16 +0400

    update little

---
+++ Pharmacy System/Migrations/20260727125049_SeedAdminUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharmacy_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "UserID", "CreatedAt", "Email", "IsActive", "PasswordHash", "Role", "UpdatedAt", "Username" },
+                values: new object[] { 1, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "admin@pharmacy.com", true, "$2a$12$BV55ozb08WyBqeRg3/jN5O1/zkmg7dHlVLDiHBb.L89srGQgAm15G", "Admin", null, "admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "UserID",
+                keyValue: 1);
+        }
+    }
+}
