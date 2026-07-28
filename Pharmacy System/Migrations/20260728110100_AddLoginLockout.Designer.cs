d55e5fe8e369135e124f28310fa4148b3cc8cfcc
Al Aiham <ayhem9987@gmail.com>
2026-07-28 15:41:25 +0400

    update the migration

--- Pharmacy System/Migrations/20260728110100_AddLoginLockout.Designer.cs
+++ Pharmacy System/Migrations/20260728110100_AddLoginLockout.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pharmacy_System;
 
@@ -11,9 +12,11 @@ using Pharmacy_System;
 namespace Pharmacy_System.Migrations
 {
     [DbContext(typeof(PharmacyContext))]
-    partial class PharmacyContextModelSnapshot : ModelSnapshot
+    [Migration("20260728110100_AddLoginLockout")]
+    partial class AddLoginLockout
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pharmacy System/Migrations/20260728110100_AddLoginLockout.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharmacy_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoginLockout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailedLoginAttempts",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockedUntil",
+                table: "users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "UserID",
+                keyValue: 1,
+                columns: new[] { "FailedLoginAttempts", "LockedUntil" },
+                values: new object[] { 0, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedLoginAttempts",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "LockedUntil",
+                table: "users");
+        }
+    }
+}
